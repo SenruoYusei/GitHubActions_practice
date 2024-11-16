1323c93f5d5d5f21bc8681efbdf6a324a78618ee
Yusei Moriwaka <senruo0827@gmail.com>
2024-11-16 17:53:12 +0900

    Revert "Apply Code Formatter"
    This reverts commit 5b915f839616e49df1819c2f7d36e992e06a7adb.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,21 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "GitHub_Actions_practice"
-copyright = "2024, Yusei Moriwaka"
-author = "Yusei Moriwaka"
+project = 'GitHub_Actions_practice'
+copyright = '2024, Yusei Moriwaka'
+author = 'Yusei Moriwaka'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ["_templates"]
+templates_path = ['_templates']
 exclude_patterns = []
+
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
-html_static_path = ["_static"]
+html_theme = 'alabaster'
+html_static_path = ['_static']
